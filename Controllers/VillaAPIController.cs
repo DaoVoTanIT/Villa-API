a5036557c0cadfcf4a41e58a1962c47bd6b70ddd
Vo Tan Dao <daovotan3@gmail.com>
2023-09-28 14:03:27 +0700

    CRUD API

--- Controllers/VillaAPIController.cs
+++ Controllers/VillaAPIController.cs
@@ -15,7 +15,7 @@ namespace Villa_API.Controllers
             return Ok(VillaStore.villaList);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetVilla")]
         // [ProducesResponseType(200)]
         // [ProducesResponseType(404)]
         // [ProducesResponseType(400)]
@@ -34,6 +34,61 @@ namespace Villa_API.Controllers
                 return NotFound();
             }
             return Ok(villa);
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<VillaDto> CreateVilla([FromBody] VillaDto villaDto)
+        {
+            // if(!ModelState.IsValid){
+            //     return BadRequest();
+            // }
+            if (VillaStore.villaList.FirstOrDefault(t => t.Name.ToLower() == villaDto.Name.ToLower()) != null)
+            {//if name is exists
+                ModelState.AddModelError("Custom model", "Villa already exists");
+                return BadRequest(ModelState);
+            }
+            if (villaDto == null)
+            {
+                return BadRequest();
+            }
+            if (villaDto.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            villaDto.Id = VillaStore.villaList.OrderByDescending(t => t.Id).FirstOrDefault().Id + 1;
+            VillaStore.villaList.Add(villaDto);
+            return CreatedAtRoute("GetVilla", new { id = villaDto.Id }, villaDto);
+        }
+        [HttpDelete]
+        public IActionResult DeleteVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            else if (VillaStore.villaList.FirstOrDefault(t => t.Id == id) == null)//neu id ko co trong store
+            {
+                return NotFound();
+            }
+            else
+            {
+                VillaStore.villaList.RemoveAll(t => t.Id == id);
+                return NoContent();
+            }
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateVilla(int id, [FromBody] VillaDto villaDto)
+        {
+            if (villaDto == null || id != villaDto.Id)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(t => t.Id == id);
+
+            villa.Name = villaDto.Name;
+            villa.Sqft = villaDto.Sqft;
+            villa.Occupancy = villaDto.Occupancy;
+            return NoContent();
         }
     }
 }
